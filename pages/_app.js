daf54020bdefb2567cf432be1d91d6456f638c04
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-11-06 13:23:24 +0200

    feat: add main layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import 'tailwindcss/tailwind.css'
 import 'styles/global.scss'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import Layout from 'components/layout'
+
+function CustomApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default CustomApp
